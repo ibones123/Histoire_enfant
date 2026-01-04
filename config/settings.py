287af5f7ac99a01a74e6dc90867635f5415f1ba2
ibrahim <ibrahim-kurdo@hotmail.fr>
2026-01-04 11:43:59 +0100

    UI)

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,9 @@ load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
-DEBUG = os.getenv("DEBUG", "0") == "1"
+#DEBUG = os.getenv("DEBUG", "0") == "1"
 
+DEBUG = True
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
